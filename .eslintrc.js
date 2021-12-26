0c2c029c6af841b743a7ff04a3dd1eb9035bf19b
Thlamz <thlamenza@hotmail.com>
2021-12-25 22:07:31 -0300

    Allowing non-null-assertions

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,11 +2,12 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   extends: ['plugin:@typescript-eslint/recommended', 'prettier'],
   parserOptions: {
-    sourceType: 'module',
+    sourceType: 'module'
   },
   rules: {
     'prettier/prettier': 'error',
     '@typescript-eslint/no-var-requires': 0,
+    '@typescript-eslint/no-non-null-assertion': 0
   },
-  plugins: ['@typescript-eslint', 'prettier'],
+  plugins: ['@typescript-eslint', 'prettier']
 };
