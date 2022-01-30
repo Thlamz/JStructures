9d53e7d697eb3e4b63efb3763b30e627b3ff3db5
Thlamz <thlamenza@hotmail.com>
2022-01-30 16:37:49 -0300

    Starting the creation of a new benchmark tool

--- src/helpers/benchmark.ts
+++ src/helpers/benchmark.ts
@@ -1,39 +1,67 @@
 const { performance } = require('perf_hooks');
 
-function limitPrecision(value: number) {
-  return value.toLocaleString(undefined, { maximumFractionDigits: 6 });
+// function limitPrecision(value: number, precision = 6) {
+//   return value.toLocaleString(undefined, { maximumFractionDigits: precision });
+// }
+
+interface BenchmarkRun {
+  name: string;
+  averageTime: number;
+  totalTime: number;
+  operations: number;
 }
 
-export function benchmark<ReturnType>(
-  fn: () => ReturnType,
-  tag: string = fn.toString()
-): ReturnType {
-  const start = performance.now();
-  const value = fn();
-  const end = performance.now();
+type OperationBenchmarker<TInput extends Array<unknown>> = (
+  name: string,
+  fn: (...value: TInput) => unknown
+) => void;
 
-  console.log(`Benchmarked ${tag} at ${limitPrecision(end - start)}ms`);
-  return value;
-}
+export class Benchmark<TInput extends Array<unknown>> {
+  private readonly _name: string;
 
-export function benchmarkAverage<Iterated, ReturnType>(
-  iterator: Array<Iterated>,
-  fn: (element: Iterated) => ReturnType,
-  tag: string = fn.toString()
-): ReturnType[] {
-  const results: ReturnType[] = [];
-  let total = 0;
-  for (const element of iterator) {
-    const start = performance.now();
-    const result = fn(element);
-    const end = performance.now();
-    results.push(result);
-    total += end - start;
+  private operations: Record<string, BenchmarkRun[]> = {};
+
+  constructor(name: string) {
+    this._name = name;
   }
-  console.log(
-    `Benchmarked ${tag} at an average of ${limitPrecision(
-      total / iterator.length
-    )}ms and total of ${limitPrecision(total)}ms`
-  );
-  return results;
+
+  benchmarkOperation(
+    operationName: string,
+    iterable: Iterable<TInput>
+  ): OperationBenchmarker<TInput> {
+    return (name: string, fn: (...value: TInput) => unknown): void => {
+      let totalTime = 0;
+      let operations = 0;
+      for (const input of iterable) {
+        const start = performance.now();
+        fn(...input);
+        const end = performance.now();
+        totalTime += end - start;
+        operations++;
+      }
+
+      if (!this.operations[operationName]) {
+        this.operations[operationName] = [];
+      }
+
+      this.operations[operationName].push({
+        name,
+        operations,
+        totalTime,
+        averageTime: totalTime / operations
+      });
+    };
+  }
+
+  get benchmarkedOperations(): string[] {
+    return Object.keys(this.operations);
+  }
+
+  operationBenchmarks(name: string): BenchmarkRun[] {
+    return this.operations[name];
+  }
+
+  get name(): string {
+    return this._name;
+  }
 }
